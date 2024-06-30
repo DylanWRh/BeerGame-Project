248f3bc1ad5e199f72f4e306669b9e0ae2fe1b8a
DylanWRh <1198914151@qq.com>
2024-06-30 13:05:09 +0800

    exp3

--- run_exp3.sh
+++ run_exp3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# python -u main.py --gameConfig=6 --maxEpisodesTrain=5000 --batchSize=128 --DQNckpt=chptexp3log1 --actionUp=2  --actionLow=-2 > exp3log1.txt
-python -u main.py --gameConfig=6 --maxEpisodesTrain=5000 --batchSize=128 --DQNckpt=chptexp3log2 --actionUp=1  --actionLow=-1 > exp3log2.txt
-python -u main.py --gameConfig=6 --maxEpisodesTrain=5000 --batchSize=128 --DQNckpt=chptexp3log3 --actionUp=3  --actionLow=-3 > exp3log3.txt
+# python -u main.py --gameConfig=6 --maxEpisodesTrain=5000 --batchSize=128 --DQNckpt=chptexp3log1 --actionUp=2  --actionLow=-2 > exp3log1
+python -u main.py --gameConfig=6 --maxEpisodesTrain=5000 --batchSize=128 --DQNckpt=chptexp3log2 --actionUp=1  --actionLow=-1 > exp3log2
+python -u main.py --gameConfig=6 --maxEpisodesTrain=5000 --batchSize=128 --DQNckpt=chptexp3log3 --actionUp=3  --actionLow=-3 > exp3log3
 
